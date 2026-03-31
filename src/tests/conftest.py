cd67fcb5aab4fab7cd30b716a750d5c36ad7a7b1
snehasaraf21 <snehasaraf21@gmail.com>
2026-03-31 19:10:43 +0100

    feat:headless updated

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,21 +1,25 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options  # <--- THIS IS THE MISSING LINE
-import os
-
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
     chrome_options = Options()
+    # Essential for GitHub Actions
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Your specific settings
     chrome_options.add_argument("--incognito")
     chrome_options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False
     })
 
+    # Selenium 4.41.0 handles the service/manager automatically
     driver = webdriver.Chrome(options=chrome_options)
-
 
     driver.maximize_window()
     driver.get("https://www.saucedemo.com/")
